4db51285bc834ca3b51d90482d1e6f213686132a
Michael Cumbow <mcumbow@comcast.net>
2021-05-06 20:25:49 -0400

    Add project files.

---
+++ ExcelExample/Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using OfficeOpenXml;
+using System.Linq;
+using ExcelExample.Models;
+
+namespace ExcelFileRead.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Obsolete]
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        [Obsolete]
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+        public ActionResult File()
+        {
+            FileUploadViewModel model = new FileUploadViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        [Obsolete]
+        public ActionResult File(FileUploadViewModel model)
+        {
+            string rootFolder = _hostingEnvironment.WebRootPath;
+            string fileName = Guid.NewGuid().ToString() + model.XlsFile.FileName;
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            FileInfo file = new FileInfo(Path.Combine(rootFolder, fileName));
+            using (var stream = new MemoryStream())
+            {
+                model.XlsFile.CopyToAsync(stream);
+                using (var package = new ExcelPackage(stream))
+                {
+                    package.SaveAs(file);
+                  //save excel file in your wwwroot folder and get this excel file from wwwroot
+                }
+            }
+            //After save excel file in wwwroot and then
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.FirstOrDefault();
+                if (worksheet == null)
+                {
+                    //return or alert message here
+                }
+                else
+                {
+                   //read excel file data and add data in  model.StaffInfoViewModel.StaffList
+                    var rowCount = worksheet.Dimension.Rows;
+                    for (int row = 1; row <= rowCount; row++)
+                    {
+                        model.StaffInfoViewModel.StaffList.Add(new StaffInfoViewModel
+                        {
+                            Name = (worksheet.Cells[row, 1].Value ?? string.Empty).ToString().Trim(),
+                            Roll = (worksheet.Cells[row, 2].Value ?? string.Empty).ToString().Trim(),
+                            Email = (worksheet.Cells[row, 3].Value ?? string.Empty).ToString().Trim(),
+                        });
+                    }
+                    
+                }
+            }
+            //return same view and  pass view model 
+            return View(model);
+        }
+    }
+}
---
+++ ExcelExample/Models/StaffInfoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcelExample.Models
+{
+    public class StaffInfoViewModel
+    {
+        public string Name { get; set; }
+        public string Roll { get; set; }
+        public string Email { get; set; }
+        public List<StaffInfoViewModel> StaffList { get; set; }
+
+        public StaffInfoViewModel()
+        {
+            StaffList = new List<StaffInfoViewModel>();
+        }
+    }
+}
